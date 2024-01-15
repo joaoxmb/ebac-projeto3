faf9a1079cbfc96390fa918dca082587619a878e
jaoxmb <joaovictorcgdossantos@gmail.com>
2024-01-15 15:44:09 -0300

    Fix: opening hours script

--- components/openingHours.js
+++ components/openingHours.js
@@ -4,7 +4,7 @@ const day = today.getDay();
 document.querySelector('#opening-hours')
   .innerHTML = `
     <h3 class="fs-4">Horários de funcionamento</h3>
-    ${day > 1 && day < 4 ? '<p class="mb-3 colored-text">Hoje nos não funcionamos!</p>' : ''}
+    ${day > 0 && day < 4 ? '<p class="mb-3 colored-text">Hoje nos não funcionamos!</p>' : ''}
     <ul>
       <li class="${day === 4 ? 'colored-text' : ''}">
         <h4 class="fs-6">Quarta e Quinta-feira</h4>
@@ -18,7 +18,7 @@ document.querySelector('#opening-hours')
         <h4 class="fs-6">Sábado</h4>
         <p class="ms-2">14h - 1h</p>
       </li>
-      <li class="${day === 1 ? 'colored-text' : ''}">
+      <li class="${day === 0 ? 'colored-text' : ''}">
         <h4 class="fs-6">Domingo</h4>
         <p class="ms-2">14h - 20h</p>
       </li>
