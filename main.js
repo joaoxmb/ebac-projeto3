64ff9c57c46a5d7ba9fe782d4e93413751f70cda
jaoxmb <joaovictorcgdossantos@gmail.com>
2024-01-15 17:11:17 -0300

    refactoring

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./lib/jquery.v3.7.1.min.js";
 import "./lib/jquery.mask.v0.9.0.min.js";
 import "./lib/bootstrap.bundle.v5.3.2.min.js";
 
-import {formValidation, formReset} from "./components/formValidation.js"
+import { formValidation, formReset } from "./components/formValidation.js"
 import parallax from "./components/parallax.js"
 import "./components/openingHours.js"
 
@@ -23,20 +23,20 @@ document.addEventListener("DOMContentLoaded", () => {
             return false;
           })
           .includes(false) ? false : true
-        
+
         if (isFullName && isValidName) {
-          return {valid: true}
+          return { valid: true }
         }
-  
-        return {valid: false, message: "O seu nome está incompleto!"}
+
+        return { valid: false, message: "O seu nome está incompleto!" }
       }
     },
     tel: {
       validation: (input, value) => {
-        if (value.length === 15 ) {
-          return {valid: true}
+        if (value.length === 15) {
+          return { valid: true }
         }
-        return {valid: false, message: "O seu número de celular está incompleto!"}
+        return { valid: false, message: "O seu número de celular está incompleto!" }
       },
       mask: (input) => {
         $(input).mask("(99) 99999-9999")
@@ -45,26 +45,26 @@ document.addEventListener("DOMContentLoaded", () => {
     email: {
       validation: (input, value) => {
         if (value.includes(".")) {
-          return {valid: true}
+          return { valid: true }
         }
-        return {valid: false, message: "Este e-mail não é válido!"}
+        return { valid: false, message: "Este e-mail não é válido!" }
       }
     },
     message: {
       validation: (input, value) => {
         if (value.length > 50) {
-          return {valid: true}
+          return { valid: true }
         }
-        return {valid: false, message: `Mensagem muito pequena! Escreva mais ${51 - value.length} caracteres.`}
+        return { valid: false, message: `Mensagem muito pequena! Escreva mais ${51 - value.length} caracteres.` }
       }
     }
-  }  
+  }
 
   formValidation(form, settings);
   form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      alert("Mensagem enviada com sucesso! Entraremos em contato assim que possivel.")
-      formReset(form);
+    e.preventDefault();
+    alert("Mensagem enviada com sucesso! Entraremos em contato assim que possivel.")
+    formReset(form);
   })
 
   // Parallax Effect
@@ -81,17 +81,17 @@ document.addEventListener("DOMContentLoaded", () => {
           opacity: ${progress};
           transform: translateY(${((i + 1) * 3) - (progress * ((i + 1) * 3))}rem);
         `
-    })
+      })
   }, 1.1)
   new parallax('#location .location-text', (element, progress) => {
     document.querySelector('#location')
       .querySelectorAll('address, #location .col-12 > p, h2, #opening-hours')
-        .forEach((e, i) => {
-          e.style = `
+      .forEach((e, i) => {
+        e.style = `
             opacity: ${progress};
             transform: translateY(${((i + 1) * 3) - (progress * ((i + 1) * 3))}rem);
           `
-        })
+      })
   })
   new parallax('.menu-transition-image', (element, progress) => {
     element
@@ -110,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
         element
           .style = `opacity: ${progress} !important`
       }, 0.7)
-  })
+    })
 
   // Warning Modal
   new bootstrap.Modal('#warning-modal').show()
